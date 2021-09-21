f0b07f9b092a93cdf8664d6fa8190452ada6ebe7
Juan Gil Sosa <juanmanuels499@gmail.com>
2021-09-21 16:53:30 -0300

    funcionamiento de las teclas para coherencia de movimiento de la serpiente

--- main.py
+++ main.py
@@ -17,26 +17,32 @@ if __name__ == '__main__':
     x1_change = 0
     y1_change = 0
 
-
-
     clock = pg.time.Clock()
 
+    # Le asignamos un valor para obtener la tecla presionada
+    key_pressed = None
     while True:
+
         # Este bucle for, es el ciclo del juego, todos lo tienen
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_LEFT:
+                print(key_pressed)
+                if (event.key == pg.K_LEFT) and (key_pressed != pg.K_RIGHT):
+                    key_pressed = pg.K_LEFT
                     x1_change = -10
                     y1_change = 0
-                if event.key == pg.K_RIGHT:
+                if (event.key == pg.K_RIGHT) and (key_pressed != pg.K_LEFT):
+                    key_pressed = pg.K_RIGHT
                     x1_change = 10
                     y1_change = 0
-                if event.key == pg.K_UP:
+                if (event.key == pg.K_UP) and (key_pressed != pg.K_DOWN):
+                    key_pressed = pg.K_UP
                     y1_change = -10
                     x1_change = 0
-                if event.key == pg.K_DOWN:
+                if (event.key == pg.K_DOWN) and (key_pressed != pg.K_UP):
+                    key_pressed = pg.K_DOWN
                     y1_change = +10
                     x1_change = 0
 
@@ -46,12 +52,15 @@ if __name__ == '__main__':
         # seteamos color, para que cuando se realice el movimiento de la serpiente
         # no deje rastro, si es que no posee length
 
-        #for i in range(snake.length):
-        #    board.new_pos(snake.color, x1 - 10, y1)
-        #    if i == snake.length:
-        #        board.set_fill((255, 255, 255))
-
-        board.new_pos(snake.color, x1, y1)
+        for i in range(snake.length):
+            #print("I es igual a ", i)
+            board.new_pos(snake.color, x1, y1)
+            #    board.new_pos(snake.color, x1 - 10, y1)
+            if i == snake.length:
+                #print("Es igual :v")
+                board.set_fill((255, 255, 255))
+        # board.set_fill((255, 255, 255))
+        # board.new_pos(snake.color, x1, y1)
 
         board.update_()
 
